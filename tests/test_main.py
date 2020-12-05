5eb8ce606c1a92b2b75e05195f9fe7d3c55c10b4
Wiktor Morawski <wiktor.morawski2000@onet.pl>
2020-12-05 15:47:53 +0100

    test_morse_coding_value_equal_ryba not passing

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,9 +5,12 @@ import unittest
 class TestMain(unittest.TestCase):
     def setUp(self):
         self.temp = Main()
-    def test_morse_coding_value_equal(self):
+    def test_morse_coding_value_equal_wiktor(self):
         expected = '.-- .. -.- - --- .-.'
         self.assertEqual(expected, self.temp.Morse_coding('wiktor'))
+    def test_morse_coding_value_equal_ryba(self):
+        expected = '.-. -.-- -... .-'
+        self.assertEqual(expected, self.temp.Morse_coding('ryba'))
     def tearDown(self):
         self.test_object = None
 
