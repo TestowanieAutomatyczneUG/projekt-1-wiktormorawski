030cd91273300f467120ec1d6772de92280e66f3
Wiktor Morawski <wiktor.morawski2000@onet.pl>
2020-12-05 17:22:57 +0100

    pass all tests to 5th

--- src/main.py
+++ src/main.py
@@ -21,5 +21,20 @@ class Main:
                 raise Exception('Contains polish letters')
         return result
     def Morse_decoding(self, morse_code):
-        return morse_code
-
+        translation = [(' ', '   '), ('A', '.-'), ('B', '-...'), ('C', '-.-.'), ('D', '-..'), ('E', '.'),
+                       ('F', '..-.'),
+                       ('G', '--.'), ('H', '....'), ('I', '..'), ('J', '.---'), ('K', '-.-'), ('L', '.-..'),
+                       ('M', '--'), ('N', '-.'),
+                       ('O', '---'), ('P', '.--.'), ('Q', '--.-'), ('R', '.-.'), ('S', '...'), ('T', '-'), ('U', '..-'),
+                       ('V', '...-'), ('W', '.--'), ('X', '-..-'), ('Y', '-.--'), ('Z', '--..'), ('0', '-----'),
+                       ('1', '.----'),
+                       ('2', '..---'), ('3', '...--'), ('4', '....-'), ('5', '.....'), ('6', '-....'), ('7', '--...'),
+                       ('8', '---..'), ('9', '----.')]
+        result = ''
+        prepared_morse_code = morse_code.split(' ')
+        for code in prepared_morse_code:
+            for trans_letter_code in translation:
+                if code == trans_letter_code[1]:
+                    result += trans_letter_code[0]
+        return result.lower()
+    
