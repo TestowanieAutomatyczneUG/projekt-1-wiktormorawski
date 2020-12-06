7026d153827e197d93fa4e1204b782143adc7d0b
Wiktor Morawski <wiktor.morawski2000@onet.pl>
2020-12-06 20:14:53 +0100

    added test_ceasar_decoding_upper_letters_with_space_start_indexes and passing

--- tests/test_main_pyhamcrest.py
+++ tests/test_main_pyhamcrest.py
@@ -59,5 +59,8 @@ class TestCeasar(unittest.TestCase):
     def test_ceasar_decoding_lower_letters_with_space_start_indexes(self):
         assert_that(self.temp.Ceasar_decoding('abc def'), equal_to('xyz abc'))
 
+    def test_ceasar_decoding_upper_letters_with_space_start_indexes(self):
+        assert_that(self.temp.Ceasar_decoding('ABC DEF'), equal_to('XYZ ABC'))
+
     def tearDown(self):
         self.temp = Main()
