b8aaa0eaf59914c7395742fdf61cf8d578d82a0e
Wiktor Morawski <wiktor.morawski2000@onet.pl>
2020-12-06 17:23:50 +0100

    doesnt pass only test_ceasar_coding_equal_values_2 TestMain all 8 pass TestCeasar 2 of 3 pass

--- tests/test_main_pyhamcrest.py
+++ tests/test_main_pyhamcrest.py
@@ -10,5 +10,8 @@ class TestCeasar(unittest.TestCase):
     def test_Instance_temp_Main(self):
         assert_that(self.temp, is_(Main))
 
-    def test_ceasar_coding_equal_values1(self):
+    def test_ceasar_coding_equal_values_1(self):
         assert_that(self.temp.Ceasar_coding('abc'), equal_to('def'))
+
+    def test_ceasar_coding_equal_values_2(self):
+        assert_that(self.temp.Ceasar_coding('def'), equal_to('ghi'))
