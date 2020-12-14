88609a602519e3b80616586519fff6c6364a659c
Wiktor Morawski <wiktor.morawski2000@onet.pl>
2020-12-14 21:27:40 +0100

    added implementation of check_result_same_length__string__and__letters_same_case

--- tests/test_main_matchers.py
+++ tests/test_main_matchers.py
@@ -28,6 +28,7 @@ def check_result_contain_single_spaces_beetween_morse(self, previous_val):
     else:
         raise Exception('Result does not contain spaces')
 
+
 add_extension(check_result_contain_single_spaces_beetween_morse)
 add_extension(check_result_length_higher_and_string_and_contain_spaces)
 
@@ -37,18 +38,34 @@ class Main_morse_coding_Matchers(unittest.TestCase):
         self.temp = Main()
 
     def test_Morse_coding_check_result_length_higher_and_string_and_contain_spaces(self):
-        argument = "wiktor 1"
+        argument = 'wiktor 1'
         assert_that(self.temp.Morse_coding(argument)).check_result_length_higher_and_string_and_contain_spaces(argument)
 
     def test_Morse_coding_check_result_contain_single_spaces_beetween_morse(self):
-        argument = "Testowanie"
+        argument = 'Testowanie'
         assert_that(self.temp.Morse_coding(argument)).check_result_contain_single_spaces_beetween_morse(argument)
+
+
+def check_result_same_length__string__and__letters_same_case(self, previous_val):
+    if len(self.val) == len(previous_val):
+        if type(self.val) == str and type(previous_val) == str:
+            if sum(1 for i in self.val if i.isupper()) == sum(1 for i in previous_val if i.isupper()):
+                return self.val
+            else:
+                raise Exception('Not Same case')
+        else:
+            raise Exception('Invalid type of argument or result of operation')
+    else:
+        raise Exception('Not Same length')
+
+add_extension(check_result_same_length__string__and__letters_same_case)
+
 
 class Main_ceasar_coding_Matchers(unittest.TestCase):
     def setUp(self):
         self.temp = Main()
 
-    def test_Ceasar_coding(self):
+    def test_Ceasar_coding_check_result_same_length__string__and__letters_same_case(self):
 
 
 if __name__ == '__main__':
